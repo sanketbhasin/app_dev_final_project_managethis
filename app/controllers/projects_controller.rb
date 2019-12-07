26f44b40d5f428c7de954588614d517a23a3abbf
Raghu Betina <raghu@firstdraft.com>
2019-12-07 22:00:55 +0000

    Only project user can modify project

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,4 +1,14 @@
 class ProjectsController < ApplicationController
+  before_action :current_user_must_be_project_user, :only => [:edit_form, :update_row, :destroy_row]
+
+  def current_user_must_be_project_user
+    project = Project.find(params["id_to_display"] || params["prefill_with_id"] || params["id_to_modify"] || params["id_to_remove"])
+
+    unless current_user == project.user
+      redirect_to :back, :alert => "You are not authorized for that."
+    end
+  end
+
   def index
     @projects = Project.all
 
